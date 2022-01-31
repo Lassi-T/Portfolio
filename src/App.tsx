6306de72e4b5bd5d834b748085ce0a11f160706f
Lassi <timoskainen.lassi@gmail.com>
2022-01-31 23:48:57 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, ReactElement } from 'react';
+import Landing from './components/Landing';
+import './App.scss';
 
-function App() {
+const App: FC = (): ReactElement => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Landing />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Landing/index.tsx
@@ -0,0 +1,49 @@
+import React, { FC, ReactElement } from 'react';
+//import { useSpring, animated } from 'react-spring';
+import Typewriter from 'typewriter-effect';
+import { ReactComponent as ColorWave } from '../../assets/svg/colorWave.svg';
+import { ReactComponent as ColorWaveBG } from '../../assets/svg/colorWaveBG.svg';
+import { ReactComponent as Lassi } from '../../assets/svg/lassi.svg';
+import { ReactComponent as Timoskainen } from '../../assets/svg/timoskainen.svg';
+import { ReactComponent as Corner } from '../../assets/svg/corner.svg';
+import './Landing.scss';
+
+const subtitleText = `Information technology student at Tampere University. Enthusiastic software developer with experience in
+full stack development. Interested in UI/UX design.`;
+
+const Landing: FC = (): ReactElement => {
+  return (
+    <div className='landing'>
+      <ColorWave className='color-wave' />
+      <ColorWaveBG className='color-wave-bg' />
+
+      <div className='landing-text'>
+        {/*<div className='landing-text__name'>
+          <Lassi className='landing-text__svg' />
+          <Timoskainen className='landing-text__svg' />
+        </div>*/}
+        <h1 className='landing-text__title'>Lassi Timoskainen</h1>
+        <div className='landing-text__subtitle'>
+          <Corner className='landing-text__subtitle corner' />
+          <h2 className='landing-text__subtitle-text'>
+            <Typewriter
+              options={{
+                cursor: '_',
+              }}
+              onInit={(typewriter) => {
+                typewriter.changeDelay(30);
+                typewriter.typeString(subtitleText).start();
+              }}
+            />
+          </h2>
+        </div>
+      </div>
+
+      <div className='scroll-indicator'>
+        <div className='scroll-indicator__wheel'></div>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
