8439e7bb2bf8290f386a51ff8bfa3680fd0acf70
Lassi <lassi.timoskainen@zoneatlas.com>
2022-02-02 14:14:29 +0200

    Added animation to about content

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -28,6 +28,7 @@ const About: FC = (): ReactElement => {
   const [mobile, setMobile] = useState(window.innerWidth <= 880);
   const [iconSize, setIconSize] = useState(window.innerWidth <= 880 ? 30 : 35);
 
+  // Page scaling
   useEffect(() => {
     window.addEventListener('resize', () => {
       setMobile(window.innerWidth <= 880);
@@ -49,8 +50,8 @@ const About: FC = (): ReactElement => {
         <div className='content content__mobile'>
           <img className='profile profile__mobile' src={profile} alt='profile' />
 
-          <div className='list__info'>
-            <div className='vertical right'>
+          <div className='list__info mobile'>
+            <div className='vertical right animation__left'>
               {textLeft.map((text, i) => {
                 return (
                   <p className='info-text' key={`info-text-${i}`}>
@@ -59,7 +60,7 @@ const About: FC = (): ReactElement => {
                 );
               })}
             </div>
-            <div className='vertical left vertical__mobile'>
+            <div className='vertical left vertical__mobile animation__left'>
               <SiCplusplus size={iconSize} />
               <FaPython size={iconSize} />
               <SiJavascript size={iconSize} />
@@ -72,7 +73,7 @@ const About: FC = (): ReactElement => {
 
             <div className='divider'></div>
 
-            <div className='vertical right vertical__mobile'>
+            <div className='vertical right vertical__mobile animation__right'>
               <FaNodeJs size={iconSize} />
               <SiExpress size={iconSize} />
               <SiMongodb size={iconSize} />
@@ -83,7 +84,7 @@ const About: FC = (): ReactElement => {
               <SiJirasoftware size={iconSize} />
             </div>
 
-            <div className='vertical left'>
+            <div className='vertical left animation__right'>
               {textRight.map((text, i) => {
                 return (
                   <p className='info-text' key={`info-text-${i}`}>
@@ -115,7 +116,7 @@ const About: FC = (): ReactElement => {
       <ColorWaveBG className='color-wave-about' />
 
       <div className='content'>
-        <div className='list__info'>
+        <div className='list__info left'>
           <div className='vertical right'>
             <SiCplusplus size={iconSize} />
             <FaPython size={iconSize} />
@@ -156,7 +157,7 @@ const About: FC = (): ReactElement => {
           </div>
         </div>
 
-        <div className='list__info'>
+        <div className='list__info right'>
           <div className='vertical right'>
             {textRight.map((text, i) => {
               return (
